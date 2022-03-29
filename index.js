4fc15167bc304c96e449ffde29fe95ab2e2a0c5c
gn-johnjoo <humanleague@i-gn.kr>
2022-03-29 17:38:45 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -2,15 +2,37 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true}));
+
+//application/json
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
 mongoose
-    .connect('mongodb+srv://johnjoo:abcd1234@cluster0.ebqcr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {})
+    .connect(config.mongoURI, {})
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요!')
+  res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+    //회원가입정보를 데이터베이스에 전달 
+    const user = new User(req.body)   
+
+    user.save((err,userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
